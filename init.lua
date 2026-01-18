50b09c04aed12e50dbfd815c154b56ff58b5e24e
Daniel Henschel <daniel@henschel.cc>
2026-01-18 15:46:13 +0100

    added typescriptreact for formatting

--- init.lua
+++ init.lua
@@ -994,7 +994,7 @@ require('lazy').setup({
         virt_text_pos = 'eol',
         mode = 'fg',
         section = { 'S', '  ', 'The color is: ', 'H' },
-        filetypes = { 'lua', 'css' },
+        filetypes = { 'typescriptreact', 'lua', 'css' },
       }
     end,
   },
